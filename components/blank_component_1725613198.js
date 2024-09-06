25c75ff5a8a890961a29568b0679c7357440ca1a
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-06 09:00:55 +0000

    Code Generation

---
+++ components/blank_component_1725613198.js
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+    <h1 class="text-6xl font-bold text-white mb-8 animate-pulse">
+      T-Rex Runner
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleComponent',
+}
+</script>
